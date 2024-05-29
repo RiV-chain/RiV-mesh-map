8ae81624b31daa82cebfe0f2d66661703f87cb43
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-05-29 17:42:32 +0300

    Public Peer port test timeout fix

--- src/main/java/org/riv/mesh/rest/PeerService.java
+++ src/main/java/org/riv/mesh/rest/PeerService.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.LinkedHashMap;
 
@@ -122,8 +123,8 @@ public class PeerService extends BaseService {
         BufferedReader in = null;
         String meta = null;
         try {
-            pingSocket = new Socket(remoteIp, port);
-            pingSocket.setSoTimeout(TIMEOUT_MS);
+            pingSocket = new Socket();
+            pingSocket.connect(new InetSocketAddress(remoteIp, port), TIMEOUT_MS);
             out = new PrintWriter(pingSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(pingSocket.getInputStream()));
             meta = in.readLine();
